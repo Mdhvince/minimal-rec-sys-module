25c7f9a706fb71fed44db80037d020ecf74661d5
Medhy Vinceslas <vinceslas.medhy@gmail.com>
2018-12-31 11:06:51 +0100

    prepare recommender.py

---
+++ recommender.py
@@ -0,0 +1,9 @@
+
+
+# create function with this in recommender.py
+#movie_content = np.array(movies.iloc[:,4:])
+#movie_content_transpose = np.transpose(movie_content)
+#dot_prod = movie_content.dot(movie_content_transpose)
+
+# arguments for make_recommendations() to feed find_similar_items():
+# df_items(movies), item_id_colname, dot_prod, item_name_colname
