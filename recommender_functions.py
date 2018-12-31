2125c346846939b97bef4ae1ccfef0b7be24385a
Medhy Vinceslas <vinceslas.medhy@gmail.com>
2018-12-31 11:06:16 +0100

    find similar implemented

--- recommender_functions.py
+++ recommender_functions.py
@@ -1,15 +1,6 @@
 import numpy as np
 import pandas as pd
 
-# create function with this in recommender.py
-#movie_content = np.array(movies.iloc[:,4:])
-#movie_content_transpose = np.transpose(movie_content)
-#dot_prod = movie_content.dot(movie_content_transpose)
-
-# arguments for make_recommendations() to feed find_similar_items():
-# df_items(movies), item_id_colname, dot_prod, item_name_colname
-
-
 
 def find_similar_items(item_id):
 	'''
