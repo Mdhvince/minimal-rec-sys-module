3183f81cae5cba4f9a92957d5785dec1eba55fe9
Medhy Vinceslas <vinceslas.medhy@gmail.com>
2018-12-31 13:15:41 +0100

    fit function implemented

--- recommender.py
+++ recommender.py
@@ -24,9 +24,132 @@ import recommender_functions as rf
 
 class Recommender():
 
-	def __init__(self, df_items, df_reviews):
+	def __init__(self, df_items, df_reviews,
+				 user_id_colname='user_id', item_id_colname='item_id',
+				 rating_col_name='rating', date_col_name='date'):
+		"""
+		Input:
+		- df_items: Pandas datafram of items
+		- df_reviews: Pandas datafram of items
+		"""
 		self.df_items = df_items
 		self.df_reviews = df_reviews
+		self.user_id_colname = user_id_colname
+		self.item_id_colname = item_id_colname
+		self.rating_col_name = rating_col_name
+		self.date_col_name = date_col_name
+
+		assert self.user_id_colname in self.df_reviews.columns, (
+			'the user_id_colname given is not in your df_reviews'
+			)
+		assert self.user_id_colname in self.df_reviews.columns, (
+			'the item_id_colname given is not in your df_reviews'
+			)
+		assert self.rating_col_name in self.df_reviews.columns, (
+			'the rating_col_name given is not in your df_reviews'
+			)
+		assert self.date_col_name in self.df_reviews.columns, (
+			'the date_col_name given is not in your df_reviews'
+			)
 
 
-	
+
+
+	def fit(self, latent_features=12, learning_rate=0.0001, iters=100):
+
+		self.latent_features = latent_features
+		self.learning_rate = learning_rate
+		self.iters = iters
+
+		# Create user-item matrix
+		user_item = self.df_reviews[[self.user_id_colname,
+									 self.item_id_colname,
+									 self.rating_col_name,
+									 self.date_col_name
+									 ]]
+
+		self.user_item_df = (
+			self.user_item.groupby(
+					[self.user_id_colname, self.item_id_colname]
+					)[self.rating_col_name].sum()
+					.unstack()
+					.reset_index()
+					.set_index(self.user_id_colname)
+					)
+		self.user_item_mat = np.array(self.user_item_df)
+
+		# Set up some useful values for later
+		self.n_users = self.user_item_mat.shape[0]
+		self.n_items = self.user_item_mat.shape[1]
+		self.num_ratings = np.count_nonzero(~np.isnan(self.user_item_mat))
+		self.user_ids_series = np.array(self.user_item_df.index)
+		self.items_ids_series = np.array(self.user_item_df.columns)
+
+		#### FunkSVD ####
+
+		# initialize the user and item matrices with random values
+		user_mat = np.random.rand(self.n_users, self.latent_features)
+		item_mat = np.random.rand(self.latent_features, self.n_items)
+
+		sse_accum = 0
+
+		print("Iterations \t\t Mean Squared Error ")
+
+		for iteration in range(self.iters):
+			old_sse = sse_accum
+			sse_accum = 0
+
+			for i in range(self.n_users):
+				for j in range(self.n_items):
+
+					# if the rating exists (so we train only on non-missval)
+					if self.user_item_mat[i, j] > 0:
+						# compute the error as the actual minus the dot 
+						# product of the user and movie latent features
+						diff = (
+							self.user_item_mat[i, j]
+							- np.dot(user_mat[i, :], item_mat[:, j])
+						)
+						# Keep track of the sum of squared errors for the 
+						# matrix
+						sse_accum += diff**2
+
+						for k in range(self.latent_features):
+							user_mat[i, k] += (
+								self.learning_rate * (2*diff*item_mat[k, j])
+							)
+
+							item_mat[k, j] += (
+								self.learning_rate * (2*diff*user_mat[i, k])
+							)
+
+			print(f"{iteration+1} \t\t {sse_accum/self.num_ratings} ")
+
+		# Keep these matrices for later
+		self.user_mat = user_mat
+		self.item_mat = item_mat
+
+		# Create ranked items
+		self.ranked_items = rf.ranked_df(self.df_reviews)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
