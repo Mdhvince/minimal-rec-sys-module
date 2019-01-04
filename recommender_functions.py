2f5b4f1e6a9566e0cea6ab0b44ec6955178346e6
Medhy Vinceslas <vinceslas.medhy@gmail.com>
2019-01-04 19:16:21 +0100

    change the find similar items by adding a window argument

--- recommender_functions.py
+++ recommender_functions.py
@@ -2,14 +2,14 @@ import numpy as np
 import pandas as pd
 
 
-def find_similar_items(item_id, df_items, item_id_colname, dot_prod):
+def find_similar_items(item_id, df_items, item_id_colname, dot_prod, window):
 
 	# find all item indices 
 	item_idx = np.where(df_items[item_id_colname] == item_id)[0][0]
     
 	# find the most similar item indices
 	# to start I said they need to be the same for all content
-	similar_id = np.where(dot_prod[item_idx] == np.max(dot_prod[item_idx]))[0]
+	similar_id = np.where( (dot_prod[item_idx] <= np.max(dot_prod[item_idx])) & (dot_prod[item_idx] >= np.max(dot_prod[item_idx])-window) )[0]
     
 	# pull the items titles based on the indices
 	similar_items = np.array(df_items.iloc[similar_id, ][item_id_colname])
